d089e5512f31e5f3cba31f622f75b7cacdc2588d
Justin Pauga <paugajt@gmail.com>
2017-10-31 19:59:14 -0600

    added static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/projects'
+
+  get 'static_pages/resume'
+
 	root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get projects" do
+    get static_pages_projects_url
+    assert_response :success
+  end
+
+  test "should get resume" do
+    get static_pages_resume_url
+    assert_response :success
+  end
+
+end
